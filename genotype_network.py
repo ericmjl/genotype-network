1374ebf81a37e921b361d40ccf50cff90e10d46b
Chevalant <kyuan94@gmail.com>
2015-06-04 17:35:41 -0400

    Generate Genotype Network Update
    Finished and tested the function to generate the graph

--- genotype_network.py
+++ genotype_network.py
@@ -1,5 +1,7 @@
 from Bio import SeqIO
 import networkx as nx
+import distance
+from itertools import combinations 
 
 class GenotypeNetwork(object):
 	"""docstring for GenotypeNetwork"""
@@ -23,10 +25,31 @@ class GenotypeNetwork(object):
 		-----
 		None
 		-----
-
 		"""
 		self.seq = SeqIO.to_dict(SeqIO.parse(filename, 'fasta'))
 
 	def generate_genotype_network(self):
 		"""
-		
+		Generates a network of genotypes that are 1 AA apart based on information from the dictionary 
+		Graph nodes: Individual HA sequences 
+		Graph edges: Indicate that two sequences are 1 AA apart
+
+		Parameters
+		-----
+		None
+		-----
+
+		Returns
+		-----
+		A genotype network from the inputted FASTA file
+		-----
+		"""
+		for seq in self.seq: #Adds a node for each sequence
+			self.G.add_node(seq)
+
+		for seq1, seq2 in combinations(self.seq, 2):
+			if distance.levenshtein(self.seq[seq1], self.seq[seq2]) == 1:
+				self.G.add_edge(seq1, seq2)
+				print(seq1, seq2)
+
+		nx.draw_networkx(self.G, with_labels = True)
