7fb4421f8428d2060557a3a1132b021c326ba004
Eric Ma <ericmajinglong@gmail.com>
2015-07-23 17:53:51 -0400

    pep8 changes

--- test_genotype_network.py
+++ test_genotype_network.py
@@ -6,23 +6,26 @@ GN.read_sequences('Test/Demo_052715.fasta')
 GN.generate_genotype_network()
 GN.write_genotype_network('Test/Demo_052715.pkl')
 
+
 def test_read_sequences_works_correctly():
-	"""
-	Checks that GN.read_sequences reads in correct number of sequences.
-	"""
-	assert len(GN.sequences) == 3
+    """
+    Checks that GN.read_sequences reads in correct number of sequences.
+    """
+    assert len(GN.sequences) == 3
+
 
 def test_generate_genotype_network():
-	"""
-	Checks that the number of nodes equals the number of sequences
-	Checks number of edges
-	"""
-	assert len(GN.sequences) == len(GN.G.nodes())
-	assert len(GN.G.edges()) == 2 #This will change based on dataset
+    """
+    Checks that the number of nodes equals the number of sequences
+    Checks number of edges
+    """
+    assert len(GN.sequences) == len(GN.G.nodes())
+    assert len(GN.G.edges()) == 2  # This will change based on dataset
+
 
 def test_write_genotype_network():
-	"""
-	Checks that the pickled network is written to disk.
-	"""
+    """
+    Checks that the pickled network is written to disk.
+    """
 
-	assert 'Demo_052715.pkl' in os.listdir('Test')
+    assert 'Demo_052715.pkl' in os.listdir('Test')
